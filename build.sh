3c03c9e0a7a0d20be44dd184728dfe9732119494
StevenBuglione <stevenbuglione1@gmail.com>
2024-11-29 17:55:57 -0500

    Update

--- build.sh
+++ build.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Install 1Password
-echo "Installing 1Password..."
-curl -LsS https://downloads.1password.com/linux/keys/1password.asc -o /etc/pki/rpm-gpg/RPM-GPG-KEY-1password
+# 1Password
+mkdir -p /var/opt
+curl https://downloads.1password.com/linux/keys/1password.asc | tee /etc/pki/rpm-gpg/RPM-GPG-KEY-1password
+sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=0\ngpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-1password" > /etc/yum.repos.d/1password.repo'
 
-cat <<EOF > /etc/yum.repos.d/1password.repo
-[1password]
-name=1Password Stable Channel
-baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch
-enabled=1
-gpgcheck=1
-repo_gpgcheck=0
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-1password
-EOF
+# Brave Browser
+curl -LsS https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -o /etc/yum.repos.d/brave-browser.repo
+curl -LsS https://brave-browser-rpm-release.s3.brave.com/brave-core.asc -o /etc/pki/rpm-gpg/brave-core.asc
 
-
-# Install Brave Browser
-BRAVE_BASEURL="https://brave-browser-rpm-release.s3.brave.com"
-KEY_BASEURL="https://brave-browser-rpm-release.s3.brave.com"
-
-echo "Installing Brave Browser..."
-curl -LsS $BRAVE_BASEURL/brave-browser.repo -o /etc/yum.repos.d/brave-browser.repo
-curl -LsS $KEY_BASEURL/brave-core.asc -o /etc/pki/rpm-gpg/brave-core.asc
-
-
-# Install additional tools
-echo "Installing additional tools..."
-rpm-ostree install screen 1password 1password-cli brave-browser && \
-    ostree container commit
-
-echo "Build script completed successfully."
+dnf install -y \
+    1password \
+    1password-cli \
+    brave-browser
 
 systemctl enable podman.socket
